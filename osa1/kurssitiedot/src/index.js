aa9acacdc1e2f11adabed3135d02bdb503d4b7bf
Tuomas Viikeri <tuomas.viikeri@gmail.com>
2020-06-10 13:42:04 +0300

    Content -> Part refakturoitu

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -12,17 +12,11 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
@@ -52,11 +46,9 @@ const App = () => {
         <Header 
           course={course}
         />
-        <Content 
-          part1={part1} exercises1={exercises1} 
-          part2={part2} exercises2={exercises2} 
-          part3={part3} exercises3={exercises3} 
-        />
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
         <Footer 
           exercises1={exercises1} 
           exercises2={exercises2} 
