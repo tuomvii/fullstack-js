dc091135b39255453ebc255997f472072ca31dcb
Tuomas Viikeri <tuomas.viikeri@gmail.com>
2020-06-17 23:46:27 +0300

    unicafe step1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Heading = (props) => <h1>{props.text}</h1> 
+
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const Statistics = (props) => <div>{props.fbtype} {props.fbstate}</div>
+
+const App = (props) => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const getGoodFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const getNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const getBadFeedback = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Heading text='give feedback' />
+
+      <Button handleClick={getGoodFeedback} text='good' />
+      <Button handleClick={getNeutralFeedback} text='neutral' />
+      <Button handleClick={getBadFeedback} text='bad' />
+
+      <Heading text='statistics' />
+      
+      <Statistics fbtype='good' fbstate={good} />
+      <Statistics fbtype='neutral' fbstate={neutral} />
+      <Statistics fbtype='bad' fbstate={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
